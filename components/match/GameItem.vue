c0230a639c7f66a193a0defffc9346db7e63817d
dailyuno <34119537+dailyuno@users.noreply.github.com>
2021-12-21 00:47:49 +0900

    아이템 및 게임 결과 추가
    BEM 방법론 적용

--- components/match/GameItem.vue
+++ components/match/GameItem.vue
@@ -1,6 +1,30 @@
 <template>
-  <div class="game-item" :class="game.isWin ? 'win' : 'lose'">
-    <div class="game-setting-info">
+  <div
+    class="match-game-item"
+    :class="game.isWin ? 'match-game-item--win' : 'match-game-item--lose'"
+  >
+    <div class="match-game-item__result">
+      <div class="match-game-item__result-type">
+        {{ game.gameType }}
+      </div>
+      <div class="match-game-item__result-time">
+        {{ getDateFromNow }}
+      </div>
+      <div
+        class="match-game-item__result-info"
+        :class="
+          game.isWin
+            ? 'match-game-item__result-info--win'
+            : 'match-game-item__result-info--lose'
+        "
+      >
+        {{ game.isWin ? "승리" : "패배" }}
+      </div>
+      <div class="match-game-item__result-duration">
+        {{ duration }}
+      </div>
+    </div>
+    <div class="match-game-item__setting">
       <div class="champion">
         <img :src="game.champion.imageUrl" alt="" />
       </div>
@@ -52,72 +76,141 @@
       <div class="game-kill-rate">
         킬관여 {{ game.stats.general.contributionForKillRate }}
       </div>
-      <div class="game-mmr">
+      <!-- <div class="game-mmr">
         <div>매치 평균</div>
         <div class="game-tier">{{ game.mmr }}</div>
-      </div>
+      </div> -->
     </div>
     <div class="game-stats-items">
-      <div
-        class="game-stats-item"
-        v-for="item in game.items"
-        :key="item.imageUrl"
-      >
-        <img :src="item.imageUrl" alt="" />
+      <div class="game-stats-items__container">
+        <div class="game-stats-item" v-for="i in 7" :key="i">
+          <img
+            v-if="i < game.items.length"
+            :src="game.items[i].imageUrl"
+            alt=""
+          />
+          <div v-else class="game-stats-no-item"></div>
+        </div>
+      </div>
+      <div class="game-stats-ward">
+        <img
+          v-if="game.isWin"
+          src="https://opgg-static.akamaized.net/images/site/summoner/icon-ward-blue.png"
+          alt=""
+        />
+        <img
+          v-else
+          src="https://opgg-static.akamaized.net/images/site/summoner/icon-ward-red.png"
+          alt=""
+        />
+        제어 와드 {{ game.stats.ward.visionWardsBought }}
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import moment from "moment";
+
 export default {
   props: {
     game: {
       type: Object,
     },
   },
+  computed: {
+    getDateFromNow() {
+      return moment(this.game.createDate, "X").fromNow();
+    },
+    duration() {
+      return moment
+        .utc(this.game.gameLength * 1000)
+        .format("mm분ss초")
+        .replace(/^0/, "");
+    },
+  },
 };
 </script>
 
 <style lang="scss">
-.game-item {
+.match-game-item {
   display: flex;
   margin-bottom: 8px;
+  height: 96px;
 
-  &.win {
+  &--win {
     background-color: #a3cfec;
     border-color: #99b9cf;
   }
 
-  &.lose {
+  &--lose {
     background-color: #e2b6b3;
     border-color: #cea7a7;
   }
-}
 
-.game-setting-info {
-  display: flex;
+  &__result {
+    width: 80px;
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: 11px;
+    color: #555;
 
-  .champion {
-    width: 46px;
-    height: 46px;
-    border-radius: 50%;
-    overflow: hidden;
+    &-type {
+      font-weight: bold;
+    }
 
-    img {
+    &-type,
+    &-time {
       width: 100%;
-      height: 100%;
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      text-align: center;
+    }
+
+    &-info {
+      font-weight: bold;
+
+      &--win {
+        color: #1a78ae;
+      }
+
+      &--lose {
+        color: #c6443e;
+      }
     }
   }
 
-  .spells,
-  .runes {
+  &__setting {
+    width: 110px;
     display: flex;
-    flex-direction: column;
-    margin-left: 4px;
+    align-items: center;
+    justify-content: center;
 
-    img {
-      width: 22px;
+    .champion {
+      width: 46px;
+      height: 46px;
+      border-radius: 50%;
+      overflow: hidden;
+
+      img {
+        width: 100%;
+        height: 100%;
+      }
+    }
+
+    .spells,
+    .runes {
+      display: flex;
+      flex-direction: column;
+      margin-left: 4px;
+
+      img {
+        width: 22px;
+      }
     }
   }
 }
@@ -128,7 +221,6 @@ export default {
   align-items: center;
   justify-content: center;
   width: 100px;
-  padding: 0 10px;
 
   .stats-kda {
     font-size: 15px;
@@ -186,6 +278,66 @@ export default {
         background-color: #8c51c5;
         border: solid 1px #7f3590;
       }
+    }
+  }
+}
+
+.game-stats-detail {
+  width: 90px;
+  font-size: 11px;
+  color: #555e5e;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
+  .game-kill-rate {
+    color: #c6443e;
+  }
+}
+
+.game-stats-items {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+
+  &__container {
+    width: 96px;
+    display: flex;
+    flex-wrap: wrap;
+
+    .game-stats-item {
+      width: 22px;
+      height: 22px;
+      border-radius: 3px;
+      margin-top: 2px;
+      margin-right: 2px;
+      overflow: hidden;
+
+      img {
+        width: 100%;
+        height: 100%;
+      }
+
+      .game-stats-no-item {
+        width: 100%;
+        height: 100%;
+        background-image: url("https://opgg-static.akamaized.net/images/pattern/opacity.1.png");
+      }
+    }
+  }
+
+  .game-stats-ward {
+    display: flex;
+    align-items: center;
+    margin-top: 7px;
+    color: #353a3a;
+    font-size: 11px;
+    text-align: center;
+
+    img {
+      margin-right: 2px;
     }
   }
 }
