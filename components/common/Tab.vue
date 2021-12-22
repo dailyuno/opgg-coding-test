416b29a4cb7373f2d4f372a8c71ddc2bb5d76b2a
dailyuno <34119537+dailyuno@users.noreply.github.com>
2021-12-23 03:36:24 +0900

    feat: Tab 컴포넌트 추가

---
+++ components/common/Tab.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="tab" v-if="isActive">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+    },
+  },
+  data() {
+    return {
+      isActive: false,
+    };
+  },
+};
+</script>
+
+<style></style>
---
+++ components/common/Tabs.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="tabs">
+    <div class="tabs__header">
+      <div
+        class="tabs__item"
+        v-for="(tab, idx) in tabs"
+        :key="idx"
+        :class="tab.isActive ? 'tabs__item--active' : ''"
+        @click="selectTab(idx)"
+      >
+        {{ tab.label }}
+      </div>
+    </div>
+    <div class="tabs__content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {},
+  data() {
+    return {
+      tabs: [],
+    };
+  },
+  created() {
+    this.tabs = this.$children;
+  },
+  mounted() {
+    this.selectTab(0);
+  },
+  methods: {
+    selectTab(i) {
+      this.tabs.forEach(tab => {
+        tab.isActive = false;
+      });
+
+      this.tabs[i].isActive = true;
+    },
+  },
+};
+</script>
+
+<style></style>
