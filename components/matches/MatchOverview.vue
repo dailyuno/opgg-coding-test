5c4cb071ff0c09b6ff9b1e8fea0b053ac5ebd607
dailyuno <34119537+dailyuno@users.noreply.github.com>
2021-12-28 00:44:23 +0900

    feat: 매치 타입 필터링 컴포넌트 추가

---
+++ components/matches/MatchOverview.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="match-overview">
+    <v-tabs v-model="tab">
+      <v-tab v-for="type in gameTypes" :key="type">
+        {{ type }}
+      </v-tab>
+    </v-tabs>
+
+    <v-tabs-items>
+      <v-tab-item>
+        <match-stats></match-stats>
+      </v-tab-item>
+    </v-tabs-items>
+  </div>
+</template>
+
+<script>
+import MatchStats from "~/components/matches/MatchStats.vue";
+export default {
+  components: {
+    MatchStats,
+  },
+  props: {},
+  data() {
+    return {
+      gameTypes: ["전체", "솔로랭크", "자유랭크"],
+    };
+  },
+};
+</script>
+
+<style></style>
--- pages/summoner/_name.vue
+++ pages/summoner/_name.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="app-layouts">
-    <div class="app-layouts__content">
+    <div class="app-layouts__container">
       <aside class="app-aside">
         <summoner-rank :league="summoner.leagues[0]"></summoner-rank>
         <summoner-rank :league="summoner.leagues[1]" small></summoner-rank>
         <summoner-most :mostInfo="mostInfo" />
       </aside>
-      <match-list :matches="matches"></match-list>
+
+      <section class="app-content">
+        <match-overview></match-overview>
+        <match-list :games="games"></match-list>
+      </section>
     </div>
   </div>
 </template>
@@ -16,19 +20,21 @@ import { mapGetters } from "vuex";
 import MatchList from "~/components/matches/MatchList";
 import SummonerRank from "~/components/summoner/SummonerRank";
 import SummonerMost from "~/components/summoner/SummonerMost";
+import MatchOverview from "~/components/matches/MatchOverview";
 
 export default {
   components: {
     MatchList,
     SummonerRank,
     SummonerMost,
+    MatchOverview,
   },
   data() {
     return {};
   },
   computed: {
     ...mapGetters("summoner", ["summoner", "mostInfo"]),
-    ...mapGetters("matches", ["matches"]),
+    ...mapGetters("matches", ["games"]),
   },
   async fetch({ store, params }) {
     try {
@@ -50,7 +56,7 @@ export default {
   background: #eaeaea;
   padding-top: 10px;
 
-  &__content {
+  &__container {
     display: flex;
     width: 1000px;
   }
