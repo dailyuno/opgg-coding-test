a27ef66ffd8cdb46dffca601e3c0caabfc0b1e17
dailyuno <34119537+dailyuno@users.noreply.github.com>
2021-12-29 15:19:08 +0900

    feat: 소환사 정보 (티어 기록, 프로필) 작업

---
+++ components/summoner/SummonerOverview.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="summoner-overview">
+    <div class="summoner-overview__badges">
+      <summoner-badge
+        v-for="tier in summoner.previousTiers"
+        :key="tier.season"
+        :tier="tier"
+      ></summoner-badge>
+    </div>
+    <summoner-profile :summoner="summoner"></summoner-profile>
+  </div>
+</template>
+
+<script>
+import SummonerBadge from "~/components/summoner/SummonerBadge.vue";
+import SummonerProfile from "~/components/summoner/SummonerProfile.vue";
+
+export default {
+  components: { SummonerBadge, SummonerProfile },
+  props: {
+    summoner: {
+      type: Object,
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.summoner-overview {
+  padding-bottom: 20px;
+
+  &__badges {
+    padding-left: 16px;
+    margin-bottom: 8px;
+  }
+}
+</style>
--- pages/summoner/_name.vue
+++ pages/summoner/_name.vue
@@ -1,15 +1,22 @@
 <template>
   <div class="app-layouts">
     <div class="app-layouts__container">
-      <aside class="app-aside">
-        <summoner-rank :league="summoner.leagues[0]"></summoner-rank>
-        <summoner-rank :league="summoner.leagues[1]" small></summoner-rank>
-        <summoner-most :mostInfo="mostInfo" />
-      </aside>
+      <div class="summoner">
+        <div class="summoner__header">
+          <summoner-overview :summoner="summoner"></summoner-overview>
+        </div>
+        <div class="summoner__content">
+          <aside class="summoner__detail">
+            <summoner-rank :league="summoner.leagues[0]"></summoner-rank>
+            <summoner-rank :league="summoner.leagues[1]" small></summoner-rank>
+            <summoner-most :mostInfo="mostInfo" />
+          </aside>
 
-      <section class="app-content">
-        <match-overview :matches="matches"></match-overview>
-      </section>
+          <section class="summoner__matches">
+            <match-overview :matches="matches"></match-overview>
+          </section>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -19,12 +26,14 @@ import { mapGetters } from "vuex";
 import SummonerRank from "~/components/summoner/SummonerRank";
 import SummonerMost from "~/components/summoner/SummonerMost";
 import MatchOverview from "~/components/matches/MatchOverview";
+import SummonerOverview from "~/components/summoner/SummonerOverview.vue";
 
 export default {
   components: {
     SummonerRank,
     SummonerMost,
     MatchOverview,
+    SummonerOverview,
   },
   data() {
     return {};
@@ -47,29 +56,40 @@ export default {
 </script>
 
 <style lang="scss">
+body {
+  background: #eaeaea;
+}
+
 .app-layouts {
   display: flex;
   justify-content: center;
-  background: #eaeaea;
   padding-top: 10px;
 
   &__container {
-    display: flex;
     width: 1000px;
   }
 }
 
-.app-aside {
-  flex: 1 0 300px;
-  max-width: 300px;
-  margin-right: 10px;
-
-  .summoner-most {
-    margin-top: 8px;
+.summoner {
+  &__header {
   }
-}
 
-.app-content {
-  flex: 1 0 690px;
+  &__content {
+    display: flex;
+  }
+
+  &__detail {
+    flex: 1 0 300px;
+    max-width: 300px;
+    margin-right: 10px;
+
+    .summoner-most {
+      margin-top: 8px;
+    }
+  }
+
+  &__matches {
+    flex: 1 0 690px;
+  }
 }
 </style>
