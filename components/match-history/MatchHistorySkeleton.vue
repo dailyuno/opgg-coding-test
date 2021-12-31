eda981aff7cdb6de1769ad457834745edec95ce3
dailyuno <34119537+dailyuno@users.noreply.github.com>
2021-12-31 16:50:34 +0900

    feat: 매치 기록 Skeleton 스타일 수정

--- components/match-history/MatchHistorySkeleton.vue
+++ components/match-history/MatchHistorySkeleton.vue
@@ -1,37 +1,35 @@
 <template>
   <div class="match-history">
-    <match-history-info-skeleton />
+    <div class="match-history-info">
+      <skeleton-loader :width="60" :height="60" />
+    </div>
+
     <match-history-setting-skeleton />
-    <match-history-score-skeleton />
-    <match-history-stats-skeleton />
+
+    <div class="match-history-score">
+      <skeleton-loader :width="80" :height="60" />
+    </div>
+
+    <div class="match-history-stats">
+      <skeleton-loader :width="60" :height="40" />
+    </div>
+
     <match-history-items-skeleton />
 
     <div class="match-history__teams">
-      <div class="match-history__team" v-for="i in 2" :key="i">
-        <skeleton-loader
-          v-for="i in 5"
-          :key="i"
-          :width="70"
-          :height="13"
-          class="mb-1"
-        />
-      </div>
+      <skeleton-loader :width="180" :height="70" />
     </div>
   </div>
 </template>
 
 <script>
-import MatchHistoryInfoSkeleton from "~/components/match-history/MatchHistoryInfoSkeleton.vue";
 import MatchHistorySettingSkeleton from "~/components/match-history/MatchHistorySettingSkeleton.vue";
-import MatchHistoryScoreSkeleton from "~/components/match-history/MatchHistoryScoreSkeleton.vue";
-import MatchHistoryStatsSkeleton from "~/components/match-history/MatchHistoryStatsSkeleton.vue";
+import MatchHistoryItemsSkeleton from "~/components/match-history/MatchHistoryItemsSkeleton.vue";
 
 export default {
   components: {
-    MatchHistoryInfoSkeleton,
     MatchHistorySettingSkeleton,
-    MatchHistoryScoreSkeleton,
-    MatchHistoryStatsSkeleton,
+    MatchHistoryItemsSkeleton,
   },
 };
 </script>
