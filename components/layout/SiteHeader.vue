8dbabdb78e415bdc252edd07e6da722d220b6806
dailyuno <34119537+dailyuno@users.noreply.github.com>
2022-01-01 03:43:55 +0900

    feat: 공통 header 추가

---
+++ components/layout/SiteHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <header class="site-header">
+    <div class="container">
+      <div class="d-flex align-items-flex-end justify-content-flex-end h-full">
+        <search-form></search-form>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+import SearchForm from "~/components/search/SearchForm.vue";
+
+export default {
+  components: { SearchForm },
+};
+</script>
+
+<style lang="scss">
+@import "site-header";
+</style>
