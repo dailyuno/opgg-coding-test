5f211b5f550e4a073baee2efe8f9c08f4b410d4a
dailyuno <34119537+dailyuno@users.noreply.github.com>
2022-01-01 19:56:24 +0900

    feat: 자동 완성 기능 추가

---
+++ components/search/SearchAutoComplete.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="search-auto-complete">
+    <search-auto-complete-item
+      v-for="summoner in summonerList"
+      :key="summoner.name"
+      :summoner="summoner"
+    ></search-auto-complete-item>
+  </div>
+</template>
+
+<script>
+import SearchAutoCompleteItem from "~/components/search/SearchAutoCompleteItem.vue";
+
+export default {
+  components: { SearchAutoCompleteItem },
+  props: {
+    summonerList: {
+      type: Array,
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+@import "search-auto-complete";
+</style>
---
+++ components/search/SearchAutoCompleteItem.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="search-auto-complete-item">
+    <nuxt-link
+      :to="{ name: 'summoner-name', params: { name: summoner.name } }"
+      class="search-auto-complete-item__link"
+    >
+      <div class="search-auto-complete-item__profile">
+        <img
+          :src="summoner.profileImageUrl"
+          alt=""
+          class="search-auto-complete-item__img"
+        />
+      </div>
+      <div class="search-auto-complete-item__detail">
+        <div class="search-auto-complete-item__name">
+          {{ summoner.name }}
+        </div>
+        <div class="search-auto-complete-item__info">
+          {{ info }}
+        </div>
+      </div>
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    summoner: {
+      type: Object,
+    },
+  },
+  computed: {
+    info() {
+      const { level, leagues } = this.summoner;
+      const [solo] = leagues;
+
+      if (solo && solo.hasResults) {
+        const { tierRank } = solo;
+        const { tier, lp } = tierRank;
+        return `${tier} - ${lp}LP`;
+      } else {
+        return `Level ${level}`;
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+@import "search-auto-complete-item";
+</style>
